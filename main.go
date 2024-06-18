eaabfc8186197ed7d62d52556cb34ad44006e970
AayushShah <aayushshah1142@gmail.com>
2024-06-18 19:35:46 +0530

    organized code

--- main.go
+++ main.go
@@ -9,7 +9,19 @@ import (
 	"os"
 )
 
-func lineCounter(r io.Reader) (int, error) {
+func byteCounter(filePath string) {
+	fi, err := os.Stat(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fi.Size())
+}
+func lineCounter(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 	// initialise an empty buffer to hold the file contents
 	buf := make([]byte, 32*1024)
 	// variable to hold line count
@@ -20,26 +32,33 @@ func lineCounter(r io.Reader) (int, error) {
 	// infinite loop
 	for {
 		// read *buffer_size* bytes from the file into the buffer, returning the number of bytes read(c)
-		c, err := r.Read(buf)
+		c, err := file.Read(buf)
 		// count number of newline characters in the bytes that have been read
 		count += bytes.Count(buf[:c], lineSep)
 
 		switch {
 		case err == io.EOF:
-			return count, nil
+			fmt.Println(count)
+			return
 
 		case err != nil:
-			return count, err
+			log.Fatal(err)
+			return
 		}
 	}
 }
 
-func countWords(r io.Reader) (int, error) {
+func countWords(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 	buf := make([]byte, 32*1024)
 	count := 0
 	inWord := false
 	for {
-		c, err := r.Read(buf)
+		c, err := file.Read(buf)
 		for i := 0; i < c; i++ {
 			if buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t' || buf[i] == '\r' {
 				if inWord {
@@ -54,51 +73,52 @@ func countWords(r io.Reader) (int, error) {
 			if inWord {
 				count++
 			}
-			return count, nil
+			fmt.Println(count)
+			return
 		}
 		if err != nil {
-			return count, err
+			log.Fatal(err)
+			return
 		}
 	}
 }
+func countBytes(filePath string) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileContents := string(data)
+	fmt.Println(len(fileContents))
+}
 
 func main() {
-	cPtr := flag.Bool("c", true, "display number of bytes of file")
-	lPtr := flag.Bool("l", true, "display number of lines")
-	wPtr := flag.Bool("w", true, "display number of words")
+	cPtr := flag.Bool("c", false, "display number of bytes of file")
+	lPtr := flag.Bool("l", false, "display number of lines")
+	wPtr := flag.Bool("w", false, "display number of words")
+	mPtr := flag.Bool("m", false, "display number of characters")
 	filePath := os.Args[len(os.Args)-1]
+
+	masterFlag := !*cPtr && !*lPtr && !*wPtr && !*mPtr
+
+	if masterFlag {
+		*cPtr = true
+		*lPtr = true
+		*wPtr = true
+		*mPtr = true
+	}
 
 	flag.Parse()
 
 	if *cPtr {
-		fi, err := os.Stat(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(fi.Size())
+		byteCounter(filePath)
 	}
 	if *lPtr {
-		file, err := os.Open(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		lines, err := lineCounter(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(lines)
+		lineCounter(filePath)
 	}
 	if *wPtr {
-		file, err := os.Open(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-		words, err := countWords(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(words)
+		countWords(filePath)
+	}
+	if *mPtr {
+		countBytes(filePath)
 	}
 }
